10a6ab68c44145e7af176f94e50aff6f0b0bed21
Ha Kwang Wook <wook@mhand.net>
2022-05-24 21:54:25 +0900

    ScoreBoardActivity 전체 화면 모드 설정

--- app/src/main/java/wook/pool/board/screen/scoreboard/ScoreBoardActivity.kt
+++ app/src/main/java/wook/pool/board/screen/scoreboard/ScoreBoardActivity.kt
@@ -2,6 +2,9 @@ package wook.pool.board.screen.scoreboard
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import wook.pool.board.R
 import wook.pool.board.base.BaseActivity
@@ -16,5 +19,23 @@ class ScoreBoardActivity: BaseActivity() {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_score_board)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun showSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, window.decorView).show(WindowInsetsCompat.Type.systemBars())
     }
 }
